992733cb95119342bd5f819ce71bd15c7cf2e7e9
Kero Filicha <kerotulu@gmail.com>
2022-10-06 19:00:45 -0700

    push pall

--- monty.h
+++ monty.h
@@ -8,7 +8,7 @@
 #include <string.h>
 #include <ctype.h>
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
+ * truct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
